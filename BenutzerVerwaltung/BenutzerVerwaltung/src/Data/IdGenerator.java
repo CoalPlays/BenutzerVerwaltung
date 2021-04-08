4ff6bd68ade97457a644402ad8f0d5b8975cd750
CoalPlays <Elias.Mattern@coop.ch>
2021-04-08 12:53:44 +0200

    BetterIdGenerator

--- BenutzerVerwaltung/BenutzerVerwaltung/src/Data/IdGenerator.java
+++ BenutzerVerwaltung/BenutzerVerwaltung/src/Data/IdGenerator.java
@@ -7,32 +7,12 @@ public class IdGenerator {
     }
 
     public String getID(){
-        String id = "";
-        String idPart = Integer.toString(ident);
-        if (ident < 10){
-            id = "00" + idPart;
-        }
-        if (9 < ident && ident < 100){
-            id = "0" + idPart;
-        }
-        if (ident > 99){
-            id += idPart;
-        }
+        String id = String.format("%03d",ident);
         ident++;
         return id;
     }
     public String getID(int presentID){
-        String id = "";
-        String idPart = Integer.toString(ident);
-        if (presentID < 10){
-            id = "00" + presentID;
-        }
-        if (9 < presentID && presentID < 100){
-            id = "0" + presentID;
-        }
-        if (ident > 99){
-            id += presentID;
-        }
+        String id = String.format("%03d",presentID);
         return id;
     }
 
